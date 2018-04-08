a2f7cfa9660b799b2340419b3c5591e22803d186
Daniel Causer <causerd@fcc.ca>
2018-04-08 14:01:35 -0400

    Test project commit

---
+++ src/HttpTracer.TestApp/Services/IDataStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HttpTracer.TestApp
+{
+    public interface IDataStore<T>
+    {
+        Task<bool> AddItemAsync(T item);
+        Task<bool> UpdateItemAsync(T item);
+        Task<bool> DeleteItemAsync(string id);
+        Task<T> GetItemAsync(string id);
+        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+    }
+}
---
+++ src/HttpTracer.TestApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HttpTracer.TestApp
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
