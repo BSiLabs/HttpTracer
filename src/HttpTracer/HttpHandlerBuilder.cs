d8e795865fe51ec4c23e7a688a909120fcbab7e6
aritchie <allan.ritchie@gmail.com>
2019-03-23 22:00:59 -0400

    Improve discoverability of structure by file separation of classes

---
+++ src/HttpTracer/HttpHandlerBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace HttpTracer
+{
+    public class HttpHandlerBuilder
+    {
+        private readonly IList<HttpMessageHandler> _handlersList = new List<HttpMessageHandler>();
+        private readonly HttpTracerHandler _rootHandler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:HttpTracer.HttpHandlerBuilder"/> class.
+        /// </summary>
+        public HttpHandlerBuilder()
+        {
+            _rootHandler = new HttpTracerHandler();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:HttpTracer.HttpHandlerBuilder"/> class.
+        /// </summary>
+        /// <param name="logger">Logger.</param>
+        public HttpHandlerBuilder(ILogger logger)
+        {
+            _rootHandler = new HttpTracerHandler(logger);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:HttpTracer.HttpHandlerBuilder"/> class.
+        /// </summary>
+        /// <param name="tracerHandler">Tracer handler.</param>
+        public HttpHandlerBuilder(HttpTracerHandler tracerHandler)
+        {
+            _rootHandler = tracerHandler;
+        }
+
+        /// <summary>
+        /// Adds a <see cref="HttpMessageHandler"/> to the chain of handlers.
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <returns></returns>
+        public HttpHandlerBuilder AddHandler(HttpMessageHandler handler)
+        {
+            if (handler is HttpTracerHandler) throw new ArgumentException($"Can't add handler of type {nameof(HttpTracerHandler)}.");
+
+            if (_handlersList.Any())
+                ((DelegatingHandler)_handlersList.LastOrDefault()).InnerHandler = handler;
+
+            _handlersList.Add(handler);
+            return this;
+        }
+
+        /// <summary>
+        /// Adds <see cref="HttpTracerHandler"/> as the last link of the chain.
+        /// </summary>
+        /// <returns></returns>
+        public HttpMessageHandler Build()
+        {
+            if (_handlersList.Any())
+                ((DelegatingHandler)_handlersList.LastOrDefault()).InnerHandler = _rootHandler;
+            else
+                return _rootHandler;
+
+            return _handlersList.FirstOrDefault();
+        }
+    }
+}
---
+++ src/HttpTracer/ILogger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HttpTracer
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
