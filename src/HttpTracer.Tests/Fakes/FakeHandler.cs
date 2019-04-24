993b53d5358770888e91049023931886c32b62ce
Chase Florell <chase.florell@gmail.com>
2019-04-24 09:38:28 -0700

    [CF] Fixes tests and constructor overloads

--- src/HttpTracer.Tests/Fakes/FakeHandler.cs
+++ src/HttpTracer.Tests/Fakes/FakeHandler.cs
@@ -6,11 +6,14 @@ namespace HttpTracer.Tests.Fakes
 {
     public class FakeHandler : DelegatingHandler
     {
+        public static readonly string FakeHeader = $"{HeaderKey}: {HeaderValue}";
+        public const string HeaderKey = "FAKE-HEADER";
+        public const string HeaderValue = "FAKE VALUE";
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
             await Task.Delay(1, cancellationToken);
-            request.Headers.Add("FAKE-HEADER", "FAKE VALUE");
+            request.Headers.Add(HeaderKey, HeaderValue);
 
             await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
 
